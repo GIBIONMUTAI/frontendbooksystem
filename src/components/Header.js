2022b5c78959c284e6ff554ef051200fb39b54be
GIBIONMUTAI <gibioncheruiyot@gmail.com>
2024-05-26 16:04:37 +0300

    updatedaddform

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
@@ -10,34 +8,48 @@ function Header() {
   const [searchTerm, setSearchTerm] = useState('');
 
   const handleSearchChange = (event) => {
-    setSearchTerm(event.target.value);
-    // Add logic here to handle search functionality 
+    setSearchTerm(event.target.value.toLowerCase());
   };
+
+  const handleSearchSubmit = (event) => {
+    event.preventDefault(); // Prevent default form submission if applicable
+
+    // Implement search logic here
+    if (searchTerm) {
+      // Perform search using searchTerm
+      console.log(`Search term: ${searchTerm}`);
+    } else {
+      console.log('Empty search term');
+    }
+  };
+
   return (
-    <div>
-      <>
-        <Navbar bg="info" data-bs-theme="info">
-          <Container>
-            <Navbar.Brand>BOOK SYSTEM</Navbar.Brand>
-            <Nav className="me-auto">
-              <Link to="/" style={{ textDecoration: 'none', paddingLeft: 13 }}>
-                Home
-              </Link>
-              <Link to="/add-book" style={{ textDecoration: 'none', paddingLeft: 13 }}>
-                Add Book
-              </Link>
-            </Nav>
-            <div className="search-container">
-              <input type="text" placeholder="Search Books..."value={searchTerm} onChange={handleSearchChange}
-                className="search-input"/>
-              <button type="button" className="search-button">
-                <FontAwesomeIcon icon={faSearch} />
-              </button>
-            </div>
-          </Container>
-        </Navbar>
-      </>
-    </div>
+    <Navbar bg="info" expand="lg">
+      <Container>
+        <Navbar.Brand>BOOK SYSTEM</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+            <Link to="/add-book" className="nav-link">
+              Add Book
+            </Link>
+          </Nav>
+          <form className="d-flex" onSubmit={handleSearchSubmit}>
+            <input
+              className="form-control me-2" type="search" placeholder="Search Books..." value={searchTerm}
+              onChange={handleSearchChange}
+              aria-label="Search"
+            />
+            <button type="submit" className="btn btn-outline-success">
+              <FontAwesomeIcon icon={faSearch} />
+            </button>
+          </form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
